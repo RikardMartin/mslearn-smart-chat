562d6b1e4a4d39c40f90f909ac247b4ec0e217ff
Richard Martin <richard.martin2@soprasteria.com>
2024-02-22 14:36:02 +0100

    - Change output format to a single json doc for each module - Update requirements - Delete unused settings

--- module_scraper/scrape_modules.py
+++ module_scraper/scrape_modules.py
@@ -7,28 +7,13 @@ import requests
 import json
 
 #%%
-response = requests.get("https://learn.microsoft.com/api/catalog/?locale=en-en&type=modules")
-content = response.json()
-modules = content["modules"]
-
-#%%
-module_urls = []
-for module in modules[:10]:
-    module_urls.append(module["url"])
-    
-    folder = module["url"].split("/modules/")[1].split('/')[0]
-    os.makedirs(os.path.join("module_data", folder), exist_ok=True)
-
-    with open(os.path.join("module_data", folder, "metadata.json"), "w") as outfile: 
-        json.dump(module, outfile)
-
-#%%
 class ModuleSpider(scrapy.Spider):
     name = "mslearn_module"
 
     def __init__(self, urls):
         super().__init__()
         self.module_urls = urls
+        # self.start_requests()
 
     def start_requests(self):
         for url in self.module_urls:
@@ -51,34 +36,60 @@ class ModuleSpider(scrapy.Spider):
         folder = rel_path[0]
         filename = f"{rel_path[1]}.txt"
 
-        with open(os.path.join("module_data", folder, filename), 'w') as f: f.write(str(page_text))
+        print(filename)
+        with open(os.path.join("module_data", folder, filename), 'w') as f:
+            f.write(str(page_text))
         # Path(os.path.join(folder, filename)).write_bytes(response.body)
         self.log(f"Saved file {filename}")
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
+
+#%%
+response = requests.get("https://learn.microsoft.com/api/catalog/?locale=en-en&type=modules")
+content = response.json()
+modules = content["modules"]
+
+#%%
+module_urls = []
+for module in modules[:3]:
+    module_urls.append(module["url"])
+    
+    folder = module["url"].split("/modules/")[1].split('/')[0]
+    path = os.path.join("module_data", folder)
+    os.makedirs(path, exist_ok=True)
+
+    with open(os.path.join(path, "metadata.json"), 'w') as outfile:
+        json.dump(module, outfile)
+
+#%%
+process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
 process.crawl(ModuleSpider, urls=module_urls)
 process.start()
 
 #%%
 process.stop()
 
+folders = os.listdir("module_data")
+folders = [file for file in folders if not '.' in file]
+for folder in folders:
+    path = os.path.join("module_data", folder)
+    files = os.listdir(path)
+    files.sort(key=lambda x: x[0])
+    files = files[:-1]
+
+    lessons = []
+    for filename in files:
+        print(filename)
+        with open(os.path.join(path, filename), 'r') as doc:
+            lessons.append(doc.read())
+            os.remove(doc.name)
+
+    with open(os.path.join(path, "metadata.json"), 'r') as metadata:
+        data = json.load(metadata)
+        data["content"] = str(lessons)
+        os.remove(metadata.name)
+        
+        with open(path+".json", 'w') as outfile:
+            json.dump(data, outfile)
+
+    os.removedirs(path)
 # %%
-for subdir, dirs, files in os.walk("module_data"):
-    print(subdir)
-    if subdir != "module_data":
-        files.sort(key=lambda x: x[0])
-        lessons = []
-        for filename in files:
-            print(filename)
-            with open(os.path.join(subdir, filename), 'r') as doc:
-                lessons.append(doc.read())
-
-        print(lessons)
-        with open(subdir+".txt", 'w') as out:
-            out.write(str(lessons))
-
-
-
-
